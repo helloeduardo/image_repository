69fd5407551370c4c241c20331bee4b5044f71fe
Eduardo Parra <helloeduardo@icloud.com>
2021-01-17 12:52:00 -0700

    Add similar image search

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -31,4 +31,19 @@ describe 'As a visitor' do
     expect(page).to have_content("Aspect Ratio: Square")
     expect(page).to have_css('.image', count: 35)
   end
+
+  it 'I can search for similar_images' do
+    visit root_path
+
+    within '#search-image' do
+      fill_in :image_url, with: "https://media3.s-nbcnews.com/i/newscms/2020_06/1535664/rainbow-stock-today-main-200205_9c367c61366578972574f9bae5f413ae.jpg"
+      click_on "Search"
+    end
+
+    expect(current_path).to eq("/search")
+
+    expect(page).to have_content("Images Similar To:")
+    expect(page).to have_css('#original-image')
+    expect(page).to have_css('.image', count: 35)
+  end
 end
