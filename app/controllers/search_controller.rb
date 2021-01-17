fe0e2aac8080e515aa9673c2de9e11304cd58a4e
Eduardo Parra <helloeduardo@icloud.com>
2021-01-16 20:07:00 -0700

    Add images to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     @text = params[:text]
-    @images = ImageFacade
+    @images = ImageFacade.images_by_text(params[:text])
   end
 end
